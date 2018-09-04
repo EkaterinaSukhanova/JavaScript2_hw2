5d4d9e68ba276ce67613b77814d6c0ee33d33c97
EkaterinaSukhanova <katrinsuh@yandex.ru>
2018-09-04 15:12:55 +0300

    Work with local host

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 'use strict';
-const BaseURL = 'http://89.108.65.123';
-const textArea = document.querySelector('.text-area');
-const myForm = document.querySelector('.text-form');
+const BaseURL = 'http://127.0.0.1:156';
+const textAreaMessage = document.querySelector('.text-area-message');
+const textAreaId = document.querySelector('.text-area-id');
+
+const myFormText = document.querySelector('.text-form');
 const myText = document.querySelector('#myText');
+
+const myFormId = document.querySelector('.id-form');
+const idUser = document.querySelector('#idUser');
+
 
 const getMessage = () => {
     fetch(`${BaseURL}/getMessage`).then((res) => {
         return res.text();
     }).then((data) => {
-        textArea.innerHTML = data;
+        textAreaMessage.innerHTML = data;
     }).catch((err) => {
         console.error(err);
     })
@@ -27,10 +33,45 @@ const sendMessage = () => {
     })
 };
 
-myForm.addEventListener('submit', (e) => {
+
+
+
+const sendIdUser = () => {
+    const data = idUser.value;
+    fetch(`${BaseURL}/user`, {
+        method: 'POST',
+        body: data,
+    }).then(() => {
+        idUser.value = '';
+        getUser(data);
+    }).catch((err) => {
+        console.error(err);
+    })
+};
+
+const getUser = (data) => {
+    fetch(`${BaseURL}/user/${data}`).then((res) => {
+        return res.text();
+    }).then((data) => {
+        textAreaId.innerHTML = data;
+    }).catch((err) => {
+        console.error(err);
+    })
+};
+
+
+
+myFormText.addEventListener('submit', (e) => {
     e.preventDefault();
     sendMessage();
 });
+
+myFormId.addEventListener('submit', (e) => {
+    e.preventDefault();
+    sendIdUser();
+});
+
 window.onload = () => {
     getMessage();
+
 };
